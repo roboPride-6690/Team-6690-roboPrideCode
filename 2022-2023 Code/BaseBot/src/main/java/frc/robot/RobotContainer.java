134f5a0247e4536138a02c6acf50c0cb135a3c33
Ishmael Kwayisi <73256035+winterguardianspyro2021@users.noreply.github.com>
2023-05-26 13:38:19 -0400

    Add files via upload

---
+++ 2022-2023 Code/BaseBot/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,149 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.autonomous.AutonomousOne;
+import frc.robot.autonomous.AutonomousTwo;
+import frc.robot.subsystems.SwerveBase;
+import frc.robot.teleop.drivebase.TeleopDrive;
+//import frc.robot.teleop.teleopArm.TeleopElevateArm;
+//import frc.robot.teleop.teleopArm.TeleopLowerArm;
+//import frc.robot.teleop.teleopArm.TeleopLowerWrist;
+//import frc.robot.teleop.teleopArm.TeleopRaiseWrist;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+//import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+//import com.revrobotics.RelativeEncoder;
+
+//import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final SwerveBase drivebase = new SwerveBase();   
+
+ 
+  // 
+  private final AutonomousOne a_Auto;    
+  private final AutonomousTwo a_Auto2;
+  private static final String kDefaultAuto = "Default"; 
+  private static final String kCustomAuto = "My Auto"; 
+  private String m_autoSelected; 
+  private final SendableChooser<Command> m_Chooser = new SendableChooser<>(); 
+  
+
+
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  CommandXboxController driverController = new CommandXboxController(0); 
+  private final Joystick driver1 = new Joystick(1);
+
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings 
+    configureBindings(); 
+    
+    //gyro = new ADXRS450_Gyro();
+    // AbsoluteDrive absoluteDrive = new AbsoluteDrive(
+    //   drivebase,
+      // Applies deadbands and inverts controls because joysticks are back-right positive while robot
+      // controls are front-left positive
+      // () -> (Math.abs(driverController.getY()) > OperatorConstants.LEFT_Y_DEADBAND) ? -driverController.getY() : 0,
+      // () -> (Math.abs(driverController.getX()) > OperatorConstants.LEFT_X_DEADBAND) ? -driverController.getX() : 0,
+      // () -> -rotationController.getX(),
+      // () -> -rotationController.getY(), true);
+
+    // AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(
+    //   drivebase,
+      // Applies deadbands and inverts controls because joysticks are back-right positive while robot
+      // // controls are front-left positive
+      // () -> (Math.abs(driverController.getY()) > OperatorConstants.LEFT_Y_DEADBAND) ? -driverController.getY() : 0,
+      // () -> (Math.abs(driverController.getX()) > OperatorConstants.LEFT_X_DEADBAND) ? -driverController.getX() : 0,
+      // () -> -rotationController.getX(),
+      // () -> -rotationController.getY(), false);
+
+    //TeleopDrive openRobotRel = new TeleopDrive(
+     // drivebase,
+     // () -> (Math.abs(driverController.getLeftX()) > 0.05) ? -driverController.getLeftX() : 0,
+     // () -> (Math.abs(driverController.getLeftY()) > 0.05) ? -driverController.getLeftY() : 0,
+     // () -> -driverController.getRightX(), () -> false, true);
+    
+    TeleopDrive closedRobotRel = new TeleopDrive(
+      drivebase,
+      () -> (Math.abs(driverController.getLeftY()) > 0.05) ? -driverController.getLeftY() : 0,
+      () -> (Math.abs(driverController.getLeftX()) > 0.05) ? -driverController.getLeftX() : 0,
+      () -> -driverController.getRightX(), () -> true, false);
+    
+    // TeleopDrive openFieldRel = new TeleopDrive(
+    //   drivebase,
+    //   () -> (Math.abs(driverController.getY()) > OperatorConstants.LEFT_Y_DEADBAND) ? -driverController.getY() : 0,
+    //   () -> (Math.abs(driverController.getX()) > OperatorConstants.LEFT_X_DEADBAND) ? -driverController.getX() : 0,
+    //   () -> -driverController.getTwist(), () -> true, true);
+
+    // TeleopDrive closedFieldRel = new TeleopDrive(
+    //   drivebase,
+    //   () -> (Math.abs(driverController.getY()) > OperatorConstants.LEFT_Y_DEADBAND) ? -driverController.getY() : 0,
+    //   () -> (Math.abs(driverController.getX()) > OperatorConstants.LEFT_X_DEADBAND) ? -driverController.getX() : 0,
+    //   () -> -driverController.getTwist(), () -> true, false);
+
+    drivebase.setDefaultCommand(closedRobotRel);   
+    a_Auto = new AutonomousOne(drivebase); 
+    a_Auto2 = new AutonomousTwo(drivebase);
+ 
+    m_Chooser.setDefaultOption("Autonomous 1", a_Auto);  
+    m_Chooser.addOption("Autonoumous 2", a_Auto2); 
+    SmartDashboard.putData(m_Chooser);
+   // registerAutos();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    driverController.button(1).onTrue((new InstantCommand(drivebase::zeroGyro))); 
+
+  }
+
+  public void setDriveMode() {
+    //drivebase.setDefaultCommand();
+  }
+  public void setMotorBrake(boolean brake) {
+    drivebase.setMotorBrake(brake);
+  }
+ 
+    /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return m_Chooser.getSelected();
+  } 
+}
+
---
+++ 2022-2023 Code/BaseBot/src/main/java/frc/robot/teleop/drivebase/TeleopDrive.java
@@ -0,0 +1,111 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.teleop.drivebase;
+
+import frc.robot.Constants.Drivebase;
+import frc.robot.subsystems.SwerveBase;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class TeleopDrive extends CommandBase {
+  private SwerveBase swerve;
+  private DoubleSupplier vX, vY, omega;
+  private BooleanSupplier driveMode; 
+  private boolean isOpenLoop; 
+  private double accelerationCounterX;
+  private double accelerationCounterY; 
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param swerve The subsystem used by this command.
+   */
+  public TeleopDrive(SwerveBase swerve, DoubleSupplier vX, DoubleSupplier vY, DoubleSupplier omega, BooleanSupplier driveMode, boolean isOpenLoop) {
+    this.swerve = swerve;
+    this.vX = vX;
+    this.vY = vY;
+    this.omega = omega;
+    this.driveMode = driveMode;
+    this.isOpenLoop = isOpenLoop; 
+    accelerationCounterX = 0;
+    accelerationCounterY = 0;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(swerve);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() { 
+    accelerationCounterX = 0;
+    accelerationCounterY = 0;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double angVelocity = 0; 
+    double xVelocity = 0;
+    double yVelocity = 0;
+    
+    //slower rotation speed for lower input values. Mr. Sanders 
+    if (omega.getAsDouble() > 0.5){
+      angVelocity = Math.pow(omega.getAsDouble(), 3) * Drivebase.MAX_ANGULAR_VELOCITY;
+    }else{
+      angVelocity = Math.pow(omega.getAsDouble(), 3) * Drivebase.MAX_ANGULAR_VELOCITY/2;
+    } 
+
+    //as long as the user holds toggle the speed will increase
+    if(vY.getAsDouble() > 0.2 || vY.getAsDouble() < -0.2) {
+      if(accelerationCounterY < Math.abs(vY.getAsDouble())){ 
+        accelerationCounterY += 0.002;
+      } 
+    } 
+    else{ 
+      accelerationCounterY = 0;  //reset acceleration back to zero.
+    }
+
+    yVelocity = Math.pow(vY.getAsDouble(), 3) * -Drivebase.MAX_SPEED * accelerationCounterY;
+
+    if(vX.getAsDouble() > 0.2 || vX.getAsDouble() < -0.2){ 
+      if(accelerationCounterX < Math.abs(vX.getAsDouble())){ 
+        accelerationCounterX += 0.002;
+      } 
+    } 
+    else{ 
+      accelerationCounterX = 0;
+    }
+      
+    xVelocity = Math.pow(vX.getAsDouble(), 3) * Drivebase.MAX_SPEED * accelerationCounterX;
+
+    SmartDashboard.putNumber("AccY: ", accelerationCounterY);
+    SmartDashboard.putNumber("AccX: ", accelerationCounterX);
+    SmartDashboard.putNumber("vX", xVelocity);
+    SmartDashboard.putNumber("vY", yVelocity);
+    //SmartDashboard.putBoolean("field centric", driveMode.getAsBoolean());
+    swerve.drive(
+      new Translation2d(
+        xVelocity,
+        yVelocity),
+      angVelocity,
+      driveMode.getAsBoolean(),
+      isOpenLoop);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
